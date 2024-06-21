36ef5f95708b650f0960f81524db6ee993ab4ad4
Tatiana Hoyos Vertel <tatiana.hoyos@globalmvm.com>
2024-06-20 19:27:41 -0500

    se implementa websocket

--- myapp/static/js/task_item.js
+++ myapp/static/js/task_item.js
@@ -39,3 +39,60 @@ function eliminarTask(){
         }
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén el elemento que contiene el nombre de usuario
+    const usernameElement = document.getElementById('username');
+    // Obtén el nombre de usuario del atributo data
+    const username = usernameElement.dataset.username;// Asegúrate de pasar el nombre de usuario desde la vista
+    nombreSinEspacios = username.replace(/\s/g, '');
+    const taskForm = document.getElementById('task-form');
+    const taskList = document.getElementById('task-list');
+
+    const wsScheme = window.location.protocol === "https:" ? "wss" : "ws";
+    const socket = new WebSocket(`${wsScheme}://${window.location.host}/ws/notifications/${nombreSinEspacios}/`);
+
+    socket.onmessage = function(event) {
+        const data = JSON.parse(event.data);
+        console.log(data)
+        showNotification(data.message);
+    };
+
+    function showNotification(message, type = 'success') {
+        Swal.fire({
+            position: 'top-end',
+            icon: type,
+            title: message,
+            showConfirmButton: true,
+        });
+    }
+
+    taskForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const title = document.getElementById('task-title').value;
+        const desc = document.getElementById('task-desc').value;
+        const assign = document.getElementById('task-assign').value;
+
+        addTask(title, desc, assign);
+        taskForm.reset();
+
+        const notificationMessage = `Nueva tarea asignada a ${assign}`;
+        socket.send(JSON.stringify({
+            'message': notificationMessage
+        }));
+    });
+
+    function addTask(title, desc, assign) {
+        fetch(`/render_task_item/?title=${title}&desc=${desc}&assign=${assign}`)
+            .then(response => response.json())
+            .then(data => {
+                const listItem = document.createElement('div');
+                listItem.innerHTML = data.html;
+                taskList.appendChild(listItem.firstChild);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                showNotification("Error al agregar la tarea", 'error');
+            });
+    }
+});
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
-# myapp/urls.py
-
-from django.urls import path
+from django.urls import path, include
+from . import routing  # Importa las rutas WebSocket de tu aplicación
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('render_task_item/', views.render_task_item, name='render_task_item'),
+    path('ws/', include(routing.websocket_urlpatterns)),  # Rutas WebSocket de tu aplicación
 ]
